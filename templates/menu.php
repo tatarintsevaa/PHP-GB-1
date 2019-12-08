ff48c83a329bf4248c77aec168f3c6c7f2d197c9
tatarintsevaa <tatalexey@gmail.com>
2019-12-09 01:05:41 +0300

    task 3 done

--- templates/menu.php
+++ templates/menu.php
@@ -1,3 +1,4 @@
  <a href="/">главная</a>
  <a href="/?page=task1">задание 1</a>
- <a href="/?page=task2">задание 2</a><br>
+ <a href="/?page=task2">задание 2</a>
+ <a href="/?page=task3">задание 3</a><br>
