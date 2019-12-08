43be7ab50c8064d6d3877d1d9b4d0687e9f6117c
tatarintsevaa <tatalexey@gmail.com>
2019-12-08 23:01:15 +0300

    done lesson5

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php 
-echo
-?>
+
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+} else {
+    $page = 'index';
+}
+
+echo renderTemplate('layout', renderTemplate('menu'), renderTemplate($page));
+
+function renderTemplate($page, $menu = '', $content = '')
+{
+    ob_start();
+    include "templates/{$page}.php";
+    return ob_get_clean();
+}
+
+
+
+
---
+++ templates/menu.php
@@ -0,0 +1,3 @@
+ <a href="/">главная</a>
+ <a href="/?page=task1">задание 1</a>
+ <a href="/?page=task2">задание 2</a><br>
