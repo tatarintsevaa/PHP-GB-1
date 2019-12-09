b9d735685c02de76aad1b10b7f39f41e89fe6dd4
tatarintsevaa <tatalexey@gmail.com>
2019-12-09 11:14:11 +0300

    task 6 done

--- templates/menu.php
+++ templates/menu.php
@@ -2,4 +2,5 @@
  <a href="/?page=task1">задание 1</a>
  <a href="/?page=task2">задание 2</a>
  <a href="/?page=task3">задание 3</a>
- <a href="/?page=task4">задание 4</a><br>
+ <a href="/?page=task4">задание 4</a>
+ <a href="/?page=task6">задание 6</a><br>
