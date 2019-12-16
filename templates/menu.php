b8cf13c47ba59123d5aecd887197830946f892c8
tatarintsevaa <tatalexey@gmail.com>
2019-12-16 10:02:19 +0300

    Выполнил 5,7,8,9 задания

--- templates/menu.php
+++ templates/menu.php
@@ -3,5 +3,8 @@
  <a href="/?page=task2">задание 2</a>
  <a href="/?page=task3">задание 3</a>
  <a href="/?page=task4">задание 4</a>
+ <a href="/?page=task5">задание 5</a>
  <a href="/?page=task6">задание 6</a>
- <a href="/?page=task7">задание 7</a><br>
+ <a href="/?page=task7">задание 7</a>
+ <a href="/?page=task8">задание 8</a>
+ <a href="/?page=task9">задание 9</a><br>
--- templates/task5.php
+++ templates/task5.php
@@ -1,8 +1,26 @@
 <?php
+$str2 = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab amet animi, ex ipsam necessitatibus totam. 
+Aut blanditiis enim excepturi fuga ipsam, laboriosam laborum, magnam mollitia officiis quam quisquam suscipit unde!';
+function translator2($str)
+{
+    $result = '';
+    for ($i = 0; $i < mb_strlen($str); $i++) {
+        $symbol = mb_substr($str, $i, 1);
+        if ($symbol == ' ') {
+            $result .= '_';
+        } else {
+            $result .= $symbol;
+        }
 
+    }
+    return $result;
+}
+function translator3($str) {
+    return str_replace(' ', '_', $str);
+}
 
 ?>
-<h1>Задание 3</h1>
+<h1>Задание 5</h1>
 <h2>
     Написать функцию, которая заменяет в строке пробелы на подчеркивания и возвращает видоизмененную строчку.
     Можно сделать через str_replace
@@ -11,6 +29,11 @@
     результат
 </h2>
 <h3>
-    <?= translator($str, $alfabet) ?>
+    вариант 1 <br>
+    <?= translator2($str2)?>
+</h3>
+<h3>
+    вариант 2 <br>
+    <?= translator3($str2)?>
 </h3>
 
