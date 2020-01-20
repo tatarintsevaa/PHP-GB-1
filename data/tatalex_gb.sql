6eae4018022b7a5f86676faf17ca72ed47e332de
tatarintsevaa <tatalexey@gmail.com>
2020-01-21 02:41:59 +0300

    Обновил дамп БД

--- data/tatalex_gb.sql
+++ data/tatalex_gb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Янв 14 2020 г., 03:34
+-- Время создания: Янв 21 2020 г., 02:40
 -- Версия сервера: 8.0.15
 -- Версия PHP: 7.3.9
 
@@ -21,6 +21,26 @@ SET time_zone = "+00:00";
 --
 -- База данных: `tatalex_gb`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `feedback` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `feedback`
+--
+
+INSERT INTO `feedback` (`id`, `name`, `feedback`) VALUES
+(1, 'Вася', 'Я первый'),
+(2, 'Петр', 'Все норм');
 
 -- --------------------------------------------------------
 
@@ -45,20 +65,48 @@ INSERT INTO `gallery` (`id`, `name`, `views`) VALUES
 (33, '04.jpg', 0),
 (34, '05.jpg', 0),
 (35, '06.jpg', 0),
-(36, '07.jpg', 0),
+(36, '07.jpg', 5),
 (37, '08.jpg', 0),
 (38, '09.jpg', 0),
 (39, '10.jpg', 0),
-(40, '11.jpg', 0),
+(40, '11.jpg', 8),
 (41, '12.jpg', 0),
 (42, '13.jpg', 0),
 (43, '14.jpg', 0),
 (44, '15.jpg', 0),
 (45, 'newImage.jpg', 0);
 
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `goods`
+--
+
+CREATE TABLE `goods` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `price` int(11) NOT NULL,
+  `image` varchar(10) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `goods`
+--
+
+INSERT INTO `goods` (`id`, `name`, `price`, `image`, `description`) VALUES
+(1, 'Картина №1', 150, '01.jpg', 'Подробное описание Картины 1'),
+(2, 'Картина №2', 100, '02.jpg', 'Подробное описание Картины 2');
+
 --
 -- Индексы сохранённых таблиц
 --
+
+--
+-- Индексы таблицы `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `gallery`
@@ -67,14 +115,32 @@ ALTER TABLE `gallery`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Индексы таблицы `goods`
+--
+ALTER TABLE `goods`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT для сохранённых таблиц
 --
+
+--
+-- AUTO_INCREMENT для таблицы `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100;
 
 --
 -- AUTO_INCREMENT для таблицы `gallery`
 --
 ALTER TABLE `gallery`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+
+--
+-- AUTO_INCREMENT для таблицы `goods`
+--
+ALTER TABLE `goods`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
